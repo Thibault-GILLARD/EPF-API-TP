535b252a7ff9defee9baced6d3ec8f86d27080bd
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-12-13 13:09:37 +0100

    Refactor API router and add data processing functions

--- services/epf-flower-data-science/main.py
+++ services/epf-flower-data-science/main.py
@@ -10,9 +10,5 @@ from src.app import get_application
 
 app = get_application()
 
-@app.get("/")
-def redirect_to_docs():
-    return RedirectResponse(url="/docs")
-
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
--- services/epf-flower-data-science/src/services/data.py
+++ services/epf-flower-data-science/src/services/data.py
@@ -2,9 +2,38 @@ import requests
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 def download_iris_dataset():
     api = KaggleApi()
     api.authenticate()
     api.dataset_download_files('uciml/iris', path='src/data', unzip=True)
 
     return {"status": "Dataset downloaded successfully"}
+
+def load_iris_dataset():
+    try:
+        dataset = pd.read_csv('src/data/Iris.csv')
+        return dataset
+    except Exception as e:
+        return {"error": f"Error loading dataset: {e}"}
+    
+def process_iris_dataset():
+    try:
+        dataset = pd.read_csv('src/data/Iris.csv')
+        dataset = dataset.drop(['Id'], axis=1)
+        return dataset
+    except Exception as e:
+        return {"error": f"Error processing dataset: {e}"}
+    
+def split_dataset(test_size):
+    try:
+        dataset = pd.read_csv('src/data/Iris.csv')
+        dataset = dataset.drop(['Id'], axis=1)
+        X = dataset.iloc[:, :-1].values
+        y = dataset.iloc[:, -1].values
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=1)
+        return {"X_train": X_train, "X_test": X_test, "y_train": y_train, "y_test": y_test}
+    except Exception as e:
+        return {"error": f"Error splitting dataset: {e}"}
