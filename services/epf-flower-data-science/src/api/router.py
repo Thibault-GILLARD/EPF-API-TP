dc6c1c87af0e22083c99a84584841e1f215142c0
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-12-20 13:06:53 +0100

    firestore

--- services/epf-flower-data-science/src/api/router.py
+++ services/epf-flower-data-science/src/api/router.py
@@ -1,10 +1,11 @@
 """API Router for Fast API."""
 from fastapi import APIRouter
 
-from src.api.routes import hello, data, analysis
+from src.api.routes import hello, data, analysis, parameters
  
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
 router.include_router(data.router, tags=["Data"])
 router.include_router(analysis.router, tags=["Analysis"])
+router.include_router(parameters.router, tags=["Analysis"])
