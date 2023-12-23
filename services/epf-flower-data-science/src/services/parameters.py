b23f0954863f3ec99bfdbbb8c9f9db68a2d42566
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-12-23 16:23:19 +0100

    try firestore param

--- services/epf-flower-data-science/src/services/parameters.py
+++ services/epf-flower-data-science/src/services/parameters.py
@@ -1,4 +1,8 @@
+import joblib
+import pandas as pd
+from src.services.data import split_dataset  # Importing the split_dataset function
 from google.cloud import firestore
+from sklearn.linear_model import LogisticRegression
 
 class FirestoreClient:
     """Wrapper around a database"""
@@ -24,3 +28,32 @@ class FirestoreClient:
         raise FileExistsError(
             f"No document found at {collection_name} with the id {document_id}"
         )
+
+def train_model(test_size):
+    try:
+        dataset = pd.read_csv('src/data/Iris.csv')
+
+        firestore_client = FirestoreClient()
+        model_params = firestore_client.get('parameters', '42iGjY3IdVqGug0uNkO5')  # Get parameters from Firestore
+
+        X_train, X_test, y_train, y_test = split_dataset(test_size)
+
+        model = LogisticRegression(**model_params)  
+
+        model.fit(X_train, y_train)  
+
+        joblib.dump(model, 'src/data/trained_model_fireparam.pkl')
+
+        return {"status": "Model trained and saved successfully"}
+
+    except Exception as e:
+        return {"error": f"Error training model: {e}"}
+
+def predict_flower(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm):
+    try:
+        model = joblib.load('src/data/trained_model_fireparam.pkl')
+        prediction = model.predict([[SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm]])
+        return {"prediction": prediction[0]}
+
+    except Exception as e:
+        return {"error": f"Error predicting flower: {e}"}
