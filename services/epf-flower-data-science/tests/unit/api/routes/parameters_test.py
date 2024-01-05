cab2d13eff4c575b9db1a75ef38cd01cbcf0d8ba
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2024-01-05 22:46:39 +0100

    try test

--- services/epf-flower-data-science/tests/unit/api/routes/parameters_test.py
+++ services/epf-flower-data-science/tests/unit/api/routes/parameters_test.py
@@ -1,14 +1,57 @@
 import pytest
-from src.services.parameters import  FirestoreClient
+from src.services.utils import FirestoreClient
+    
+# Unit tests for the FirestoreClient class
 
-def test_firestore_client():
-    firestore_client = FirestoreClient()
+class TestFirestoreClient:
+    @pytest.fixture
+    def firestore_client(self) -> FirestoreClient:
+        """
+        Test client for integration tests
+        """
+        return FirestoreClient()
 
-    try:
+    def test_get(self, firestore_client):
+        # Setup some test data
         collection_name = "parameters"
-        document_id = "your_document_id_here"
-        result = firestore_client.get(collection_name, document_id)
-        assert result is not None  # Check if the result is not None
-    except Exception as e:
-        pytest.fail(f"Error: {e}")
+        document_id = "test_document"
+
+        # Call the function to be tested
+        response = firestore_client.get(collection_name, document_id)
+
+        # Assert the output
+        assert response == {
+            "test_param": "test_value"
+        }
+        
+    def test_add(self, firestore_client):
+        # Setup some test data
+        collection_name = "parameters"
+        data = {
+            "test_param": "test_value"
+        }
+
+        # Call the function to be tested
+        response = firestore_client.add(collection_name, data)
+
+        # Assert the output
+        assert response == {
+            "message": "Document added with ID: test_document"
+        }
+        
+    def test_update(self, firestore_client):
+        # Setup some test data
+        collection_name = "parameters"
+        document_id = "test_document"
+        data = {
+            "test_param": "test_value"
+        }
+
+        # Call the function to be tested
+        response = firestore_client.update(collection_name, document_id, data)
+
+        # Assert the output
+        assert response == {
+            "message": "Document updated with ID: test_document"
+        }
         
